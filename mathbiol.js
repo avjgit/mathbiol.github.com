8ee5e237fc80da835ed8ae7bc043d3d10a63dd98
AJ <avjmail@gmail.com>
2017-06-28 21:17:30 +0300

    added machine learning functions

--- mathbiol.js
+++ mathbiol.js
@@ -377,6 +377,73 @@ mathbiol.load=function(md){
         
     console.log('loading')
 }
+
+var machineLearningInput;
+var machineLearningOutput;
+
+mathbiol.getExampleData = function() {
+
+    var young = 0; 
+    var old = 1;
+
+    var illLight = 0; 
+    var illSevere =1;
+
+    var outInHours = [1, 0, 0];
+    var outInDays = [0, 1, 0];
+    var outInMonths = [0, 0, 1];
+
+    return [
+        [ [young, illLight],    outInHours ], // young, not severe illness -> out in hours
+        [ [young, illSevere],   outInDays ], // young, severe illness -> out in days
+        [ [old, illLight],      outInDays ], // old, not severe illness -> out in days
+        [ [old, illSevere],     outInMonths ], // old, severe illness -> out in weeks
+    ];
+}
+
+// first - learn from data
+mathbiol.learnStayLength = function(trainingData) {
+    
+    // settings - depends on data
+    var inputCount = 2; // age, illness
+    var outputCount = 3; // out in hours, days, weeks
+    var learningRate = 0.4; // trial and error? how to set in unsupervised?
+    var trainSteps = 1000;
+
+    function train(data) {
+        for (var i = 0; i < data.length; i++) {
+            machineLearningInput.activate(data[i][0]);
+            machineLearningOutput.activate();
+            machineLearningOutput.propagate(learningRate, data[i][1]);
+        }
+    }
+
+    function retrain(data) {
+        for (var i = 0; i < trainSteps; i++) {
+            train(_.shuffle(data));
+        }
+    }
+
+    machineLearningInput = new synaptic.Layer(inputCount);
+    machineLearningOutput = new synaptic.Layer(outputCount);
+    machineLearningInput.project(machineLearningOutput); // map machineLearningInputs to machineLearningOutput
+    retrain(trainingData); // train
+}
+
+// second - get machineLearningOutput from trained model
+mathbiol.getStayLength = function (testData) {
+    machineLearningInput.activate(testData);
+
+    var result = machineLearningOutput.activate();
+
+    console.log("Hours neuron: " + result[0] * 100 + "%");
+    console.log("Days neuron: " + result[1] * 100 + "%");
+    console.log("Weeks neuron: " + result[2] * 100 + "%");
+
+    // todo: normalize (split to make 100% sum)
+
+}
+
 mathbiol.load.about='load &lt;module&gt; to load a module specified in load.json'
 mathbiol.load.help='This command will load <a href="load.json" target="_blank">registered modules</a> by name and can also load unregistered modules by url'
 
